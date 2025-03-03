1d9e6762d737ac7fe60aeee074611338e3fdd3f5
Ewoukouskous <nino.lefebvre@ynov.com>
2025-03-03 16:51:03 +0100

    feat(Exercise): Loops
    Done the "for" loop for printing all the elements of the array

---
+++ src/fr/ynov/java/easy/Arrays.java
@@ -0,0 +1,15 @@
+package fr.ynov.java.easy;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+
+        System.out.println("For : ");
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ", ");
+        }
+    }
+
+}
