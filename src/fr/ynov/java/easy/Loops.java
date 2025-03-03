ebdecb974a5a8c46551b6e8a563142ef1f51e734
Ewoukouskous <nino.lefebvre@ynov.com>
2025-03-03 16:42:22 +0100

    feat(Exercise): Loops
    Done the "for" loop

---
+++ src/fr/ynov/java/easy/Loops.java
@@ -0,0 +1,21 @@
+package fr.ynov.java.easy;
+
+public class Loops {
+
+    public static void main(String[] args) {
+        System.out.println("For : ");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("-=-=-=-=-=-=-");
+
+//        System.out.println("While : ");
+//
+//        int index = 1;
+//        while (index <= 10) {
+//
+//        }
+    }
+
+}
