213d96943a94a22b01ba20177611c50ecdca8346
Ewoukouskous <nino.lefebvre@ynov.com>
2025-03-08 21:51:00 +0100

    fix(clarity output): add separators
    add separator to make the console more clean and understandable

--- src/fr/ynov/java/game/Game.java
+++ src/fr/ynov/java/game/Game.java
@@ -1,7 +1,5 @@
 package fr.ynov.java.game;
 
-import java.util.Scanner;
-
 public class Game {
 
     public static void game() {
@@ -9,18 +7,19 @@ public class Game {
         Player player = new Player("temp", 0);
         Utils.newPlayer(player);
         System.out.println("Welcome " +  player.getName());
-        System.out.println("-=-=-=-=-=-=-=-=-=-=-");
 
         boolean revenge = true;
 
         while (revenge) {
 
-            Choice.Choices playerChoice = Choice.playerChoice();
             System.out.println("-=-=-=-=-=-=-=-=-=-=-");
+            Choice.Choices playerChoice = Choice.playerChoice();
 
             Choice.Choices computerChoice = Choice.computerChoice();
+            System.out.println("-=-=-=-=-=-=-=-=-=-=-");
 
             System.out.println("Your choice: " + playerChoice + "\nComputer choice: " + computerChoice);
+            System.out.println("-=-=-=-=-=-=-=-=-=-=-");
 
             if (Utils.checkRound(playerChoice, computerChoice)) {
                 player.addScore();
